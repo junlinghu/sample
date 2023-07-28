b2412ce4722c7d26a22d75cf0b3db9eeef5fcbf8
junlinghu <junlinghu@gmail.com>
2023-07-28 13:09:48 -0700

    Update classify.py

--- classify.py
+++ classify.py
@@ -19,28 +19,59 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Using device:', device)
 
 from sentence_transformers import SentenceTransformer
-model = SentenceTransformer('sentence-transformers/multi-qa-distilbert-cos-v1')
-model.to(device)
-
 import numpy as np
 
 def generate_embedding(file_name, npy_file):
+    model = SentenceTransformer('sentence-transformers/multi-qa-distilbert-cos-v1')
+    model.to(device)
+    
     df = pd.read_csv(file_name, sep='\t')
     sentences = [str(row['title']) + '. ' + row['text'] for _, row in df.iterrows()]
     embeddings = model.encode(sentences)
     np.save('data/' + npy_file, embeddings)
 
 import subprocess
-
 def download_file(url):
     command = "wget {}".format(url)
     subprocess.run(command, shell=True)
     
+from torch import nn
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.get_layer = nn.Sequential(
+                            nn.Linear(18, 20),
+                            nn.ReLU(),
+                            nn.Linear(20, 10),
+                            nn.ReLU(),
+                            nn.Linear(10, 2),)
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.get_layers(x)
+        return logits
+
+def classify():
+    model = NeuralNetwork().to(device)
+    print(model)
+
+    X = torch.rand(1, 18, device=device)
+    logits = model(X)
+    pred_probab = nn.Softmax(dim=1)(logits)
+    y_pred = pred_probab.argmax(1)
+    print(f"Predicted class: {y_pred}")
+
 if __name__ == '__main__':
 	#file_name = "test_number.csv"
 	#load_csv(file_name)
     # file_name = "test_text.csv"
     # npy_file = "test.npy"
     # generate_embedding(file_name,npy_file)
-    url="https://www.dropbox.com/s/648ddugqtimjgxp/churn.csv"
-    download_file(url)
+    
+    # url="https://www.dropbox.com/s/648ddugqtimjgxp/churn.csv"
+    # download_file(url)
+    
+    classify()
+    
+    
