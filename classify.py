0e3f855342913947501befa54865d5cbeaba62b6
junlinghu <junlinghu@gmail.com>
2023-07-28 09:50:39 -0700

    Update classify.py

--- classify.py
+++ classify.py
@@ -30,21 +30,9 @@ def generate_embedding(file_name, npy_file_name):
     embeddings = model.encode(sentences)
     np.save('data/' + npy_file_name, embeddings)
 
-def load_csv_text(file_name):
-    df = pd.read_csv(file_name, sep='\t')
-    for i, row in df:
-        x1=int(row['x1'])
-        x2=int(row['x2'])
-        plus=x1+x2
-        total.append(plus)
-        print(i, x1, x2, plus)
-    df['total'] = total
-    df.to_csv("test_new.csv", sep='\t', index=False)
-
-
 if __name__ == '__main__':
 	#file_name = "test_number.csv"
 	#load_csv(file_name)
     file_name = "test_text.csv"
     npy_file = "test.npy"
-    load_csv_text(file_name)
+    generate_embedding(file_name,npy_file)
